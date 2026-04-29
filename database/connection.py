6327b465f11f8de18b22326b9fbef4e424e1aaff
Elcio <moreiraelcio32@gmail.com>
2026-04-29 15:51:37 -0300

    Corrigido connection via env

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,9 @@
 import os
 import psycopg2
 
+
 def conectar():
     return psycopg2.connect(
-        "postgresql://controlev2_user:ZYlcDCsbPYSHCZeSEYjzXgn5dfRIO7qN@dpg-d7ofi2e7r5hc73b1jtlg-a/controlev2",
-        
+        os.environ["postgresql://controlev2_user:ZYlcDCsbPYSHCZeSEYjzXgn5dfRIO7qN@dpg-d7ofi2e7r5hc73b1jtlg-a.virginia-postgres.render.com/controlev2"],
+        sslmode="require"
     )
-
-#sslmode="require"
