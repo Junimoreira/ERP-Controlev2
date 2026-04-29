4c2a9594431e010d0d7dbb056b89f6232a4783cb
Elcio <moreiraelcio32@gmail.com>
2026-04-29 16:09:07 -0300

    Fix DATABASE_URL env

--- database/connection.py
+++ database/connection.py
@@ -4,6 +4,6 @@ import psycopg2
 
 def conectar():
     return psycopg2.connect(
-        os.environ["postgresql://controlev2_user:ZYlcDCsbPYSHCZeSEYjzXgn5dfRIO7qN@dpg-d7ofi2e7r5hc73b1jtlg-a.virginia-postgres.render.com/controlev2"],
+        os.environ["DATABASE_URL"],
         sslmode="require"
     )
