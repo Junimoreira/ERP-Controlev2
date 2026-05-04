54befed5cd69179ef8368ab6ef23e46f09a17a92
junimoreira <moreiraelcio32@gmail.com>
2026-05-04 01:14:50 -0300

    Removendo arquivos de teste

--- teste.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-
-try:
-    conn = psycopg2.connect("postgresql://controlev2_user:ZYlcDCsbPYSHCZeSEYjzXgn5dfRIO7qN@dpg-d7ofi2e7r5hc73b1jtlg-a.virginia-postgres.render.com/controlev2")
-    print("Conectado com sucesso!")
-except Exception as e:
-    print(e)
